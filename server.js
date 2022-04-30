91ecd4b6165d24d866b6c9ccde7d3e26ec0cb095
autumnet <54563144+autumnet@users.noreply.github.com>
2022-04-30 03:33:46 -0400

    added comments about get functions

--- server.js
+++ server.js
@@ -12,28 +12,34 @@ const server = app.listen(port,() => {
     console.log('App is running on port %PORT%'.replace('%PORT%',port))
 })
 
+//at /app endpoint: 'OK' message
 app.get('/app/', (req, res) => {
     res.status(200).end('OK')
     res.type('text/plain')
 })
 
+// at /app/flip endpoint: results of one coin flip
 app.get('/app/flip/', (req, res) => {
     res.status(200).json({ "flip" : coinFlip()})
 })
 
+// at /app/flips/# endpoint: report flip results and summary
 app.get('/app/flips/:number', (req, res) => {
     var flips = coinFlips(req.params.number)
     res.status(200).json({ "raw" : flips, "summary" : countFlips(flips)})
 })
 
+// at /app/flip/call/heads: user guesses heads, report results of a coin flip and compare to call
 app.get('/app/flip/call/heads', (req, res) => {
     res.status(200).json(flipACoin("heads"))
 })
 
+// at /app/flip/call/heads: user guesses tails, report results of a coin flip and compare to call
 app.get('/app/flip/call/tails', (req, res) => {
     res.status(200).json(flipACoin("tails"))
 })
 
+// default endpoint gives 404 error and message 
 app.use(function(req, res) {
     res.status(404).end("Endpoint does not exist")
     res.type("text/plain")
